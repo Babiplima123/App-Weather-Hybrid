12fd88fb7c866a8405cf208bd4b672502b7a5419
Bárbara Ann Lima <barbaraannoportunidades@outlook.com>
2023-09-23 18:49:29 -0300

    to-correct-future

---
+++ obj/Debug/net7.0-android/android/src/crc64695f8a6c7ccd3222/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64695f8a6c7ccd3222;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Weather_App_Mobile_Hybrid.MainActivity, Weather-App-Mobile-Hybrid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Weather_App_Mobile_Hybrid.MainActivity, Weather-App-Mobile-Hybrid", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Weather_App_Mobile_Hybrid.MainActivity, Weather-App-Mobile-Hybrid", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
